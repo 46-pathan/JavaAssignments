849451ad2b7abb72095784825a688b442fa51b4f
46-pathan <aarefapathan164@gmail.com>
2024-09-11 20:52:49 +0530

    add divisiblenumber file

---
+++ DivisibleNumber.java
@@ -0,0 +1,17 @@
+package basicassignments;
+
+//Write a Java program to print numbers between 1 to 100 which are divisible by 3, 5 
+//and by both. 
+
+public class DivisibleNumber {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%3==0||i%5==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
